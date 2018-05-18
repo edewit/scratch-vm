3dccd7a46f704fcefd6f265c1ce1f94f32362c36
Erik Jan de Wit <erikjan.dewit@gmail.com>
2018-05-18 17:17:54 +0200

    initial version

--- src/extension-support/extension-manager.js
+++ src/extension-support/extension-manager.js
@@ -23,6 +23,7 @@ const builtinExtensions = {
     ev3: () => require('../extensions/scratch3_ev3'),
     makeymakey: () => require('../extensions/scratch3_makeymakey'),
     boost: () => require('../extensions/scratch3_boost'),
+    minecraft: () => require('../extensions/scratch3_minecraft'),
     gdxfor: () => require('../extensions/scratch3_gdx_for')
 };
 
